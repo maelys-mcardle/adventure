d6025d289ded8126bf6626e614d3d445053082c7
Maëlys McArdle <airseal@hotmail.com>
2017-10-18 18:48:43 -0400

    Fixed parsing of text, added comments.

--- src/parser/rawtostory.js
+++ src/parser/rawtostory.js
@@ -31,17 +31,34 @@ function parseRawText(rawText) {
 
   let key;
 
+  // The markdown parser provides an array of items. The first is the
+  // string "markdown".
+  if (rawText.contents.length === 0 || 
+      rawText.contents[0] !== "markdown") {
+    return stateText;
+  }
+
+  // Go through all the items in the array provided by the
+  // markdown parser. These are the headers and paragraphs.
   for (let entry of rawText.contents.slice(1)) {
 
     let entryType = entry[0];
+
+    // Header.
     if (entryType === "header") {
       let headerLevel = entry[1].level;
       let headerText = entry[2];
       key = headerText;
+    
+    // Paragraph. 
     } else if (entryType === "para" && key) {
       let paragraphText = entry[1];
+
+      // This is the second paragraph onward under the header.
       if (key in stateText.text) {
         stateText.text[key] += '\n' + paragraphText;
+
+      // This is the first paragraph under the header.
       } else {
         stateText.text[key] = paragraphText;
       }
