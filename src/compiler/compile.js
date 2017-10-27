5b9994c11561beff63c98ea6d5a20b0015a738e4
Maëlys McArdle <airseal@hotmail.com>
2017-10-26 23:48:09 -0400

    Added CLI to compiler.

--- src/compiler/compile.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const loadStory = require('./loadstory');
-
-loadStory.loadJson('samples/simple').then(story => {
-  console.log(story)
-}).catch(reason => console.log(reason))
---
+++ src/compiler/compilercli.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const loadStory = require('./loadstory/loadstory');
+
+const argv = require('yargs')
+  .usage('Usage: $0 [options]')
+  .example('$0 -d samples/simple', 'load the story in the simple dir')
+  .alias('d', 'directory')
+  .nargs('d', 1)
+  .describe('d', 'Load a story from the directory')
+  .demandOption(['d'])
+  .help('h')
+  .alias('h', 'help')
+  .argv;
+
+loadStory.loadJson(argv.directory).then(story => {
+  console.log(story);
+}).catch(errorReason => console.log(errorReason));
+
