ac9de7856bb4eeb7419f3d574bfb957f4b5c559a
Maëlys McArdle <airseal@hotmail.com>
2017-12-11 19:24:18 -0500

    Added jsdoc.

--- src/engine/load/file/loadfiles.js
+++ src/engine/load/file/loadfiles.js
@@ -10,12 +10,22 @@ module.exports = {
   load: loadFiles
 };
 
+/**
+ * Returns all the files in a given directory.
+ * @param {sring} rootDirectory The directory path.
+ * @returns {File[]} All the files in the directory.
+ */
 function loadFiles(rootDirectory) {
   let filePaths = getFilePaths(rootDirectory);
   let files = getFileContents(rootDirectory, filePaths);
   return files;
 }
 
+/**
+ * Returns all the paths for files in a given directory.
+ * @param {sring} rootDirectory The directory path.
+ * @returns {string[]} All the paths for the files in the directory.
+ */
 function getFilePaths(rootDirectory) {
 
   let filePaths = [];
@@ -36,6 +46,12 @@ function getFilePaths(rootDirectory) {
   return filePaths;
 }
 
+/**
+ * Returns all the files with their contents in a given directory.
+ * @param {sring} rootDirectory The directory path.
+ * @param {sring[]} filePaths All the paths for files in the directory.
+ * @returns {File[]} All the files in the directory.
+ */
 function getFileContents(rootDirectory, filePaths) {
 
   let files = [];
