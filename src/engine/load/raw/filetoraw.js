86563e54980f9ea4fa7938bebc31596d32392a71
Maëlys McArdle <airseal@hotmail.com>
2017-12-15 20:41:46 -0500

    Added jsdocs.

--- src/engine/load/raw/filetoraw.js
+++ src/engine/load/raw/filetoraw.js
@@ -11,6 +11,11 @@ module.exports = {
   parse: parseFiles
 };
 
+/**
+ * Parses the story files and parses them in an intermediary format.
+ * @param {File[]} storyFiles The files and their contents.
+ * @returns {RawStory} The story in an intermediary format.
+ */
 function parseFiles(storyFiles) {
   let storyActions = {}
   let storyEntities = {}
@@ -43,22 +48,39 @@ function parseFiles(storyFiles) {
   return new RawStory(storyConfig, storyActions, storyEntities);
 }
 
+/**
+ * Parses the story config file.
+ * @param {File} file The original story config file and its contents.
+ * @returns {Object} The parsed story config.
+ */
+function parseStoryConfigFile(file) {
+  return yaml.load(file.contents);
+}
+
+/**
+ * Parses an action file and parses it in an intermediary format.
+ * @param {Object} actions A map containing all the parsed actions.
+ * @param {File} file The original action file and its contents.
+ * @returns {Object} The updated actions map.
+ */
 function parseActionFile(actions, file) {
-  let key = actionIdentifier(file);
+  let key = getActionIdentifier(file);
   let value = yaml.load(file.contents);
   
   actions[key] = new RawAction();
-  actions[key].name = actionName(file);
-  actions[key].path = actionPath(file);
+  actions[key].name = getActionName(file);
+  actions[key].path = getActionPath(file);
   actions[key].action = new RawParsedFile(file.name, value);
 
   return actions;
 }
 
-function parseStoryConfigFile(file) {
-  return yaml.load(file.contents);
-}
-
+/**
+ * Parses an entity file and parses it in an intermediary format.
+ * @param {Object} entities A map containing all the parsed entities.
+ * @param {File} file The original entity file and its contents.
+ * @returns {Object} The updated entities map.
+ */
 function parseEntityFile(entities, file) {
   
   if (file.isYaml()) {
@@ -77,15 +99,23 @@ function parseEntityFile(entities, file) {
   return entities;
 }
 
+/**
+ * Parses an entity file and parses it in an intermediary format.
+ * @param {Object} entities A map containing all the parsed entities.
+ * @param {File} file The original entity file and its contents.
+ * @param {function} parseFunction The function to parse the file.
+ * @param {string} type The file type.
+ * @returns {Object} The updated entities map.
+ */
 function parseEntityFileWithFunction(entities, file, parseFunction, type) {
-  let key = entityIdentifier(file);
+  let key = getEntityIdentifier(file);
   let value = parseFunction(file.contents);
   let parsedFile = new RawParsedFile(file.name, value);
 
   if (!(key in entities)) {
     entities[key] = new RawEntity();
-    entities[key].path = entityPath(file);
-    entities[key].name = entityName(file);
+    entities[key].path = getEntityPath(file);
+    entities[key].name = getEntityName(file);
   }
   
   entities[key][type].push(parsedFile);
@@ -93,26 +123,56 @@ function parseEntityFileWithFunction(entities, file, parseFunction, type) {
   return entities;
 }
 
-function entityIdentifier(file) {
-  return entityPath(file) + constants.PATH_SEP + entityName(file);
+/**
+ * Returns an identifier for the entity.
+ * @param {File} file An entity file.
+ * @returns {string} The combined path and name of the entity.
+ */
+function getEntityIdentifier(file) {
+  return [getEntityPath(file), getEntityName(file)].join(constants.PATH_SEP);
 }
 
-function entityPath(file) {
+/**
+ * Returns the entity path.
+ * @param {File} file An entity file.
+ * @returns {string} The entity path.
+ */
+function getEntityPath(file) {
   return file.directory.slice(1, -1).join(constants.PATH_SEP);
 }
 
-function entityName(file) {
+/**
+ * Returns the entity name.
+ * @param {File} file An entity file.
+ * @returns {string} The entity name.
+ */
+function getEntityName(file) {
   return file.directory.slice(-1)[0];
 }
 
-function actionIdentifier(file) {
-  return actionPath(file) + constants.PATH_SEP + actionName(file);
+/**
+ * Returns an identifier for the action.
+ * @param {File} file An action file.
+ * @returns {string} The combined path and name of the action.
+ */
+function getActionIdentifier(file) {
+  return [getActionPath(file), getActionName(file)].join(constants.PATH_SEP);
 }
 
-function actionPath(file) {
+/**
+ * Returns the action path.
+ * @param {File} file An action file.
+ * @returns {string} The action path.
+ */
+function getActionPath(file) {
   return file.directory.slice(1).join(constants.PATH_SEP);
 }
 
-function actionName(file) {
+/**
+ * Returns the action name.
+ * @param {File} file An action file.
+ * @returns {string} The action name.
+ */
+function getActionName(file) {
   return file.name;
 }
